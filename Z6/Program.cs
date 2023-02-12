099db5fd31bfe9d4ad0cca76fa2298d3007a010c
Evgeny <drjackq@yandex.ru>
2023-02-12 17:42:11 +0300

    Решили задачу №6

---
+++ Z6/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите число ");
+int a = int.Parse(Console.ReadLine()!);
+if (a%2==0)
+{
+    Console.WriteLine("Это число четное");
+}
+else
+{
+    Console.WriteLine("Это число нечетное");
+}
