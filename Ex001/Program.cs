3be69ba1dba6bd30ed76afdbeb232489a0804f85
Evgeny <drjackq@yandex.ru>
2023-02-12 16:33:27 +0300

    Создали программу по нахождению квадрата введенного числа

--- Ex001/Program.cs
+++ Ex001/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите число: ");
+int a = int.Parse(Console.ReadLine());
+int b = a * a;
+Console.WriteLine("Квадрат вашего числа - ");
+Console.Write(b);
+
+
+
+
