fb62d762389f6914698e4033213bba24abd9e0b8
Evgeny <drjackq@yandex.ru>
2023-02-12 17:12:20 +0300

    Cкоректировали программу по нахождению максимально, минимального или равных чисел

--- Ex002/Program.cs
+++ Ex002/Program.cs
@@ -4,9 +4,15 @@ Console.Write("Введите второе  число ");
 int b = int.Parse(Console.ReadLine()!);
 if (a > b)
 {
-    Console.Write(a);
+    Console.WriteLine("Максимальное число: " + a);
+    Console.WriteLine("Минимальное число: " + b);
 }
-if (a < b)
+else if (a < b)
 {
-    Console.Write(b);
+    Console.WriteLine("Максимальное число: " + b);
+    Console.WriteLine("Минимальное число: " + a);
+}
+if (a == b)
+{
+    Console.Write("Эти числа равны");
 }
