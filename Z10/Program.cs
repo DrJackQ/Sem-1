ffada0c8a85998a6d0951753e28bd78d3f5a961a
Evgeny <drjackq@yandex.ru>
2023-02-18 21:27:46 +0300

    Задача 10

---
+++ Z10/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Clear();
+int num = new Random().Next(100, 1000); // num 25
+int a1 = num / 10; // a1 =25 / 10 = 2
+int cent = a1 % 10;
+Console.WriteLine($"Средняя цифра числа {num} это {cent}");
+
