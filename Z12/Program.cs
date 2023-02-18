b679fdfa237c16775e3faab2410eebb2d6d2a0a9
Evgeny <drjackq@yandex.ru>
2023-02-18 17:50:31 +0300

    Задача 12

---
+++ Z12/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите Певрое число ");
+int a = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите Второе число ");
+int b = int.Parse(Console.ReadLine()!);
+if (a % b == 0)
+{
+    Console.WriteLine($"{a}, {b} -> кратно");
+}
+else
+{
+    Console.WriteLine($"{a}, {b} -> не кратно, остаток {a % b} ");
+}
